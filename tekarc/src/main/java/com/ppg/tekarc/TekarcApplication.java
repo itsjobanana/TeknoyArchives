0fc3bf555b71f528b37468a84315b84b04776627
Joann Alfante <joann.alfante@cit.edu>
2022-11-15 20:50:39 +0800

    sample

---
+++ tekarc/src/main/java/com/ppg/tekarc/TekarcApplication.java
@@ -0,0 +1,13 @@
+package com.ppg.tekarc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TekarcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TekarcApplication.class, args);
+	}
+
+}
---
+++ tekarc/src/test/java/com/ppg/tekarc/TekarcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ppg.tekarc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TekarcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
